1de2fbc6d83879e570e85ce77ce808acb0c09760
gleeebkaaa <145246329+gleeebkaaa@users.noreply.github.com>
2025-07-22 10:03:08 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+TELEGRAM_TOKEN = "8062969502:AAHw52lkhUfmtgos5Rb3U76XZVpGYJ5eaJs"  # Токен от BotFather
+GITHUB_TOKEN = "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"      # Персональный токен GitHub
+REPO_OWNER = "gleeebkaaa"        # Ваш GitHub-логин
+REPO_NAME = "photostk"             # Имя репозитория
+ALLOWED_USERS_FILE = "allowed_users.txt"  # Список разрешенных пользователей
